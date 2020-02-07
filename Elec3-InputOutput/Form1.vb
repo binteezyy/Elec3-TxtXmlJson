71e6121e142b40867087f3d22cd60ffc9997dafc
unknown <alvinpanganiban22@gmail.com>
2020-02-07 20:35:15 +0800

    requirement 02-08-20

---
+++ Elec3-InputOutput/Form1.vb
@@ -0,0 +1,44 @@
+﻿Imports System.IO
+Imports Newtonsoft.Json
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim project_dir As String = Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory()).ToString()).ToString()
+        File.Create(project_dir + "\Test.txt").Dispose()
+
+        Dim file_dir As String = project_dir + "\Test.txt"
+        Dim fileExists As Boolean = File.Exists(file_dir)
+        Using sw As New StreamWriter(File.Open(file_dir, FileMode.OpenOrCreate))
+            sw.WriteLine(TextBox1.Text.ToString() + "," + TextBox2.Text.ToString() + "," + TextBox3.Text.ToString())
+        End Using
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim project_dir As String = Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory()).ToString()).ToString()
+        Dim name As String = TextBox1.Text.ToString()
+        Dim age As String = TextBox2.Text.ToString()
+        Dim address As String = TextBox3.Text.ToString()
+
+        Dim xmlDeclaration As New XDeclaration("1.0", "UTF-8", "yes")
+        Dim document As XDocument =
+            New XDocument(xmlDeclaration,
+                            New XElement("TestFile",
+                                New XElement("name", name),
+                                New XElement("age", age),
+                                New XElement("address", address)))
+        document.Save(project_dir + "\test.xml")
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim project_dir As String = Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory()).ToString()).ToString()
+        Dim dict As New Dictionary(Of String, String)
+        dict.Add("name", TextBox1.Text.ToString())
+        dict.Add("age", TextBox2.Text.ToString())
+        dict.Add("address", TextBox3.Text.ToString())
+
+        File.WriteAllText(project_dir & "\test.json", JsonConvert.SerializeObject(dict, NewtonSoft.Json.Formatting.Indented))
+    End Sub
+
+    Private Sub TextBox2_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox2.KeyPress
+        e.Handled = Not (Char.IsDigit(e.KeyChar) Or e.KeyChar = ".")
+    End Sub
+End Class
---
+++ Elec3-InputOutput/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Elec3-InputOutput")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Elec3-InputOutput")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("07dfb182-c8a9-4c1c-83d3-7eb67b457f67")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
